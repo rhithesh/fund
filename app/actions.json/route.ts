222d9af7fd169cacfaf39992e186c9a91fe229ea
hithesh <hithesh@Hitheshs-MacBook-Air.local>
2024-09-16 06:55:41 +0530

    push

--- app/actions.json/route.ts
+++ app/actions.json/route.ts
@@ -3,9 +3,8 @@ import { ACTIONS_CORS_HEADERS, ActionsJson } from "@solana/actions";
 export const GET = async () => {
   const payload: ActionsJson = {
     rules: [
-      // Map all root level routes to an action
       {
-        pathPattern: "/",
+        pathPattern: "/**",
         apiPath: "/api/donation/",
       },
     ],
